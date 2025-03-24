95e050879e67562fbd5202b61f3dd473a4ddb3f6
Bikmacs <macsim.shmelev@yandex.ru>
2025-03-24 10:38:34 +0500

    Основная программа(готовая)

---
+++ 1rabota/Controllers/HomeController1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _1rabota.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // Возвращает представление Index.cshtml
+        }
+
+        public IActionResult Page2()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            string message = "Hello " + name + ", NumTimes is: " + numTimes;
+            return message;
+        }
+    }
+}
